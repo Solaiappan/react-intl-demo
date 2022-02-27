549fff8785de4233ee278a8b983116da695935f1
Solaiappan Rajendran <srajendran@netskope.com>
2022-02-27 14:23:04 +0100

    committing changes

--- src/components/ComponentInjection/code.js
+++ src/components/ComponentInjection/code.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MyComponent = () => {
   return (
     <code>
-      {"const translatedTitle = intl.formatMessage(module1['button-title']);"}
+      {"<FormattedMessage {...module3.componentInjection} values={{ break2: (<Fragment><br /><br /></Fragment>),"}
       <br/>
       {"'link-to-google': (...chunks) => (<a href=\"https://www.google.com\">{chunks}</a>)}}/>"}
     </code>
